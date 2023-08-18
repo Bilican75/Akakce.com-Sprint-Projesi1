054e59580643e3400e0ef25669504b1ab987ea0b
zeynepgurel <gurelzeynep21@gmail.com>
2023-08-18 20:36:00 +0200

    User story 2 üzerinde calisiliyor

--- src/Zeynep3.java
+++
@@ -1,5 +0,0 @@
-public class Zeynep3 {
-    public static void main(String[] args) {
-        System.out.println("User story 3 giris yapildi");
-    }
-}
---
+++ src/Zeynep_US_03.java
@@ -0,0 +1,9 @@
+import org.junit.Test;
+
+public class Zeynep_US_03 {
+    @Test
+
+    public void Test3(){
+
+    }
+}
