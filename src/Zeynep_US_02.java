5f8b179e77ce27e29c7677d74abe6de9c1e02d3d
zeynepgurel <gurelzeynep21@gmail.com>
2023-08-18 20:53:29 +0200

    User story 2 calisiyor su an

--- src/Zeynep_US_02.java
+++ src/Zeynep_US_02.java
@@ -7,6 +7,7 @@ public class Zeynep_US_02 {
         System.out.println("User story2");
         System.out.println("US2");
         System.out.println("user story 2 in progress");
+        System.out.println("US 2 calisiyor");
 
     }
 }
