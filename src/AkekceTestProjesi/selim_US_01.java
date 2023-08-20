84f2aef445f218ee64260a740182ab7a616771f3
selimOzal <selimozalnl@gmail.com>
2023-08-21 01:22:03 +0200

    akakce.com hesap olsuturma kodlari yazildi

--- src/AkekceTestProjesi/selim_US_01.java
+++ src/AkekceTestProjesi/selim_US_01.java
@@ -1,10 +1,79 @@
 package AkekceTestProjesi;
 
+import org.asynchttpclient.util.Assertions;
+import org.junit.Assert;
 import org.junit.Test;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.support.ui.Select;
 
 public class selim_US_01 {
     @Test
     public void test01(){
+
+        WebDriver driver= new ChromeDriver();
+
+        driver.get("https://www.akakce.com/");
+
+        WebElement hesapAc= driver.findElement(By.xpath("//*[@id=\"H_rl_v8\"]/a[1]"));
+
+        hesapAc.click();
+
+        WebElement ad= driver.findElement(By.xpath("//input[@id='rnufn']"));
+        ad.sendKeys("Selim");
+
+        WebElement soyad=driver.findElement(By.xpath("//input[@id='rnufs']"));
+        soyad.sendKeys("Yilmaz");
+
+        WebElement email=driver.findElement(By.xpath("//input[@id='rnufe1']"));
+
+        email.sendKeys("grup16basarir@gmail.com");
+
+        WebElement emailT=driver.findElement(By.xpath("//input[@id='rnufe2']"));
+
+        emailT.sendKeys("grup16basarir@gmail.com");
+
+        WebElement sifre= driver.findElement(By.xpath("//input[@id='rnufp1']"));
+        sifre.sendKeys("1234567Ss");
+
+        WebElement sifreT= driver.findElement(By.xpath("//input[@id='rnufp2']"));
+        sifreT.sendKeys("1234567Ss");
+
+        WebElement cinsiyetE= driver.findElement(By.xpath("//input[@id='rngm']"));
+        cinsiyetE.click();
+
+        WebElement ilsec=driver.findElement(By.xpath("//*[@id='locpr']"));
+
+        Select il=new Select(ilsec);
+        il.selectByIndex(1);
+
+
+
+        WebElement webGun=driver.findElement(By.xpath("//*[@id='bd']"));
+        WebElement webAy=driver.findElement(By.xpath("//*[@id='bm']"));
+        WebElement webYil=driver.findElement(By.xpath("//*[@id='by']"));
+
+        Select day=new Select(webGun);
+        Select month=new Select(webAy);
+        Select year=new Select(webYil);
+
+        day.selectByVisibleText("10");
+        month.selectByValue("5");
+        year.selectByVisibleText("1990");
+
+        WebElement KullanimSoz= driver.findElement(By.xpath("//input[@id='rnufpca']"));
+        KullanimSoz.click();
+
+        WebElement BilgilendirmeButonu= driver.findElement(By.xpath("//input[@id='rnufnee']"));
+        BilgilendirmeButonu.click();
+
+        WebElement hesapacma=driver.findElement(By.xpath("//input[@id='rfb']"));
+        hesapacma.click();
+
+
+
 
 
 
